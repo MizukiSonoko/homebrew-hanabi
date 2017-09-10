7a6bd6ab55e222d590d18e5edc57c617ba5fd4ad
mizukisonoko <mizuki.sonoko@gmail.com>
2017-09-10 18:03:14 +0900

    Refactoring hanabi.rb

--- hanabi.rb
+++ hanabi.rb
@@ -2,16 +2,15 @@
 class Hanabi < Formula
   desc "Display hanabi"
   homepage "https://github.com/MizukiSonoko/hanabi"
-  url "https://github.com/MizukiSonoko/Hanabi/archive/v1.0.tar.gz" 
-  version "1.0"
+  url "https://github.com/MizukiSonoko/Hanabi/archive/v1.0.tar.gz"
   sha256 "4884ceb222632f9c4a7a7f58395ac472dd9b1b771719f421d40073a72676d0bc"
   head "https://github.com/MizukiSonoko/Hanabi.git"
 
   def install
-    system "./configure","--disable-debug",
-                         "--disable-dependency-tracking",
-                         "--disable-silent-rules",
-                         "--prefix=#{prefix}"
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}"
     system "make"
     bin.install "src/hanabi"
   end
