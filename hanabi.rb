26a017ba4a09786341f640eb34962d5c95941798
mizukisonoko <mizuki.sonoko@gmail.com>
2017-09-10 16:28:16 +0900

    Fix sha256 '4884ceb222632f9c4a7a7f58395ac472dd9b1b771719f421d40073a72676d0bc'

--- hanabi.rb
+++ hanabi.rb
@@ -3,7 +3,7 @@ require "formula"
 class Hanabi < Formula
   homepage "https://github.com/MizukiSonoko/hanabi"
   url "https://github.com/MizukiSonoko/Hanabi/archive/v1.0.tar.gz" 
-  sha256 "452e90e3c1d5aa4f6c685c5a30508f8b609205539fbd402e0f21752cb6ca0c44" 
+  sha256 "4884ceb222632f9c4a7a7f58395ac472dd9b1b771719f421d40073a72676d0bc"
   head "https://github.com/MizukiSonoko/Hanabi.git"
   version "1.0"
 
