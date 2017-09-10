f77fd2fbe3d43f3509ff5b80b3f16f2c2403b067
mizukisonoko <mizuki.sonoko@gmail.com>
2017-09-10 17:55:44 +0900

    Refactoring hanabi.rb

--- hanabi.rb
+++ hanabi.rb
@@ -1,10 +1,9 @@
-require "formula"
 
 class Hanabi < Formula
   desc "Display hanabi"
   homepage "https://github.com/MizukiSonoko/hanabi"
-  version "1.0"
   url "https://github.com/MizukiSonoko/Hanabi/archive/v1.0.tar.gz" 
+  version "1.0"
   sha256 "4884ceb222632f9c4a7a7f58395ac472dd9b1b771719f421d40073a72676d0bc"
   head "https://github.com/MizukiSonoko/Hanabi.git"
 
